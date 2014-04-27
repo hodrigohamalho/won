3f35130548eecb84c4d320bebbb9809944b9d0c5
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2014-04-26 22:30:49 -0300

    schedule logic to get metrics with Quartz

---
+++ src/main/java/won/task/MetricsListener.java
@@ -0,0 +1,25 @@
+//package won.task;
+//
+//import javax.inject.Inject;
+//import javax.servlet.ServletContextEvent;
+//import javax.servlet.ServletContextListener;
+//
+///**
+// * @author Rodrigo Ramalho
+// *         hodrigohamalho@gmail.com.
+// */
+////public class MetricsListener implements ServletContextListener {
+//
+////    @Inject
+////    private CollectMetricsTask metrics;
+////
+////    @Override
+////    public void contextInitialized(ServletContextEvent servletContextEvent) {
+////        metrics.setTimer(5000);
+////    }
+////
+////    @Override
+////    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+////        System.out.println("Testando tb!");
+////    }
+//}
