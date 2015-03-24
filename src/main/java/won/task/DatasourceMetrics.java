68c5da24e99e38041182221d0856cee1a9c54b5e
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2015-03-24 01:42:50 -0300

    removing scheduler code

--- src/main/java/won/task/DatasourceMetrics.java
+++ src/main/java/won/task/DatasourceMetrics.java
@@ -1,18 +1,18 @@
-package won.task;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
-/**
- * @author Rodrigo Ramalho
- *         hodrigohamalho@gmail.com.
- */
-public class DatasourceMetrics implements Job {
-
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("Executando o job :)");
-    }
-
-}
+//package won.task;
+//
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//
+///**
+// * @author Rodrigo Ramalho
+// *         hodrigohamalho@gmail.com.
+// */
+//public class DatasourceMetrics implements Job {
+//
+//    @Override
+//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        System.out.println("Executando o job :)");
+//    }
+//
+//}
--- src/main/java/won/task/MetricsCollector.java
+++ src/main/java/won/task/MetricsCollector.java
@@ -1,57 +1,57 @@
-package won.task;
-
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.impl.StdSchedulerFactory;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
-
-/**
- * @author Rodrigo Ramalho
- *         hodrigohamalho@gmail.com.
- */
-@Startup
-@Singleton
-public class MetricsCollector {
-
-    @PostConstruct
-    private void startup(){
-        Scheduler scheduler = null;
-        try {
-            scheduler = StdSchedulerFactory.getDefaultScheduler();
-
-            JobDetail datasource = newJob(DatasourceMetrics.class).build();
-
-            // Trigger the job to run now, and then repeat every 40 seconds
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(simpleSchedule()
-                    .withIntervalInSeconds(40)
-                    .repeatForever())
-                    .build();
-
-            scheduler.scheduleJob(datasource, trigger);
-            scheduler.start();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    @PreDestroy
-    private void shutdown(){
-        System.out.println("SHUTDOWN!!!");
-    }
-
-
-}
+//package won.task;
+//
+//import org.quartz.JobDetail;
+//import org.quartz.Scheduler;
+//import org.quartz.SchedulerException;
+//import org.quartz.Trigger;
+//import org.quartz.impl.StdSchedulerFactory;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
+//import javax.ejb.Singleton;
+//import javax.ejb.Startup;
+//
+//import static org.quartz.JobBuilder.newJob;
+//import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+//import static org.quartz.TriggerBuilder.newTrigger;
+//
+///**
+// * @author Rodrigo Ramalho
+// *         hodrigohamalho@gmail.com.
+// */
+//@Startup
+//@Singleton
+//public class MetricsCollector {
+//
+//    @PostConstruct
+//    private void startup(){
+////        Scheduler scheduler = null;
+////        try {
+////            scheduler = StdSchedulerFactory.getDefaultScheduler();
+////
+////            JobDetail datasource = newJob(DatasourceMetrics.class).build();
+////
+////            // Trigger the job to run now, and then repeat every 40 seconds
+////            Trigger trigger = newTrigger()
+////                    .startNow()
+////                    .withSchedule(simpleSchedule()
+////                    .withIntervalInSeconds(40)
+////                    .repeatForever())
+////                    .build();
+////
+////            scheduler.scheduleJob(datasource, trigger);
+//            scheduler.start();
+//        } catch (SchedulerException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+//
+//    @PreDestroy
+//    private void shutdown(){
+//        System.out.println("SHUTDOWN!!!");
+//    }
+//
+//
+//}
